fe020948cf7c2ab0849e3a0a1dc07ad2934a3675
vhphuc <pirent420@gmail.com>
2014-07-29 09:24:49 +0700

    Updating POM no container profiles

---
+++ arquillian-starter/src/main/java/com/github/pirent/Greeter.java
@@ -0,0 +1,14 @@
+package com.github.pirent;
+
+import java.io.PrintStream;
+
+public class Greeter {
+
+	public void greet(PrintStream to, String name) {
+		to.println(createGreeting(name));
+	}
+	
+	public String createGreeting(String name) {
+		return "Hello, " + name + "!";
+	}
+}
